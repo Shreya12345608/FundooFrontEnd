4edcf841c84a1076c1da674a06ba840d0735fc59
Shreya <malviyashreya26@gmail.com>
2021-08-15 20:14:43 +0530

    [Shreya]Add: :triangular_flag_on_post: SIgnUP page using angular  :monocle_face:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgetComponent } from './Components/forget/forget.component';
 import { LoginComponent } from './Components/login/login.component';
+import { ResetComponent } from './Components/reset/reset.component';
+import { SignupComponent } from './Components/signup/signup.component';
 
-const routes: Routes = [{ path: 'Login', component: LoginComponent }];
+const routes: Routes = [{ path: 'Login', component: LoginComponent },
+{ path: 'signup', component: SignupComponent },
+{ path: 'forget', component: ForgetComponent },
+{ path: 'reset', component: ResetComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
