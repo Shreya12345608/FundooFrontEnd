e5624d1dc77fd6e2c9e34f6a3809e30070efb837
Shreya <malviyashreya26@gmail.com>
2021-08-17 13:14:20 +0530

    [Shreya]refactored  :triangular_flag_on_post:  show and hide password by using toggle button  FundooNote using angular :monocle_face:

--- src/app/Components/signup/signup.component.ts
+++ src/app/Components/signup/signup.component.ts
@@ -9,23 +9,28 @@ import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
 export class SignupComponent implements OnInit {
   registerForm!: FormGroup;
     submitted = false;
+    // variable - default false
+show: boolean = false;
+
     
   constructor(private formBuilder: FormBuilder) { }
 
+  // click event function toggle
+password() {
+  this.show = !this.show;
+}
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
-      firstName: new FormControl('', [Validators.required,
-        Validators.pattern('^[A-Z][a-z]{2,}$')
-       ] ,),       
-      lastName: ['', Validators.required],
-     email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required],
-
+ firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(12), Validators.pattern('[a-zA-Z ]*')]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(12), Validators.pattern('[a-zA-Z ]*')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]],
+      confirmPassword: ['', Validators.required]
   // },{
   //   validator: MustMatch('password', 'confirmPassword')
   });
 }
+
 // convenience getter for easy access to form fields
 get f() { return this.registerForm.controls; }
 
